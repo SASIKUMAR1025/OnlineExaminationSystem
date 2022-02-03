8754f7b239e09f8d1c7d1a33d849305f0c900b33
SASIKUMAR1025 <72292578+SASIKUMAR1025@users.noreply.github.com>
2022-02-03 16:53:28 +0530

    Create HOME PAGE.php

---
+++ HOME PAGE.php
@@ -0,0 +1,18 @@
+<?php
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+$email=$_POST["email"];
+$birthday=$_POST["birthday"];
+$number=$_POST["number"];
+$clgname=$_POST["clgname"];
+$branch=$_POST["branch"];
+$cgpa=$_POST["cgpa"];
+echo "<p>In the FNAME text box,you entered: $fname </p>
+       <p>In the LNAME text box,you entered: $lname </p>
+       <p>In the EMAIL text box,you entered: $email</p>
+       <p>In the BIRTHDAY text box,you entered: $birthday</p>
+       <p>In the number text box,you entered: $number </p>
+       <p>In the CLGNAME text box,you entered: $clgname </p>
+       <p>In the BRANCH text box,you entered: $branch </p>
+       <p>In the CGPA text box,you entered: $cgpa</p>";
+?>
