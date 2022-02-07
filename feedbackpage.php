6adf99268db6c8f38410744b488da842622676bf
SASIKUMAR1025 <72292578+SASIKUMAR1025@users.noreply.github.com>
2022-02-07 15:10:42 +0530

    Create feedbackpage.php

---
+++ feedbackpage.php
@@ -0,0 +1,8 @@
+<?php
+$firstname=$_POST["firstname"];
+$mailid=$_POST["mailid"];
+$subject=$_POST["subject"];
+echo "<p>In the FNAME text box,you entered: $firstname</p>
+       <p>In the EMAIL text box,you entered: $mailid</p>
+      <p>In the COMMENTS text box,you entered: $subject</p>";
+?>
