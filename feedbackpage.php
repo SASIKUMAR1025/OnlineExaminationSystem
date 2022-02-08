b68ede78db88c5a2f72bdb617bd97943c27066c4
DigvijaySawale <67545979+DigvijaySawale@users.noreply.github.com>
2022-02-08 18:09:39 +0530

    Delete feedbackpage.php

--- feedbackpage.php
+++
@@ -1,8 +0,0 @@
-<?php
-$firstname=$_POST["firstname"];
-$mailid=$_POST["mailid"];
-$subject=$_POST["subject"];
-echo "<p>In the FNAME text box,you entered: $firstname</p>
-       <p>In the EMAIL text box,you entered: $mailid</p>
-      <p>In the COMMENTS text box,you entered: $subject</p>";
-?>
