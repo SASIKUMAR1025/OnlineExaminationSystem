f154da6699fc2e0d10d501c9f2fc02fe6ef05754
SASIKUMAR1025 <72292578+SASIKUMAR1025@users.noreply.github.com>
2022-02-09 15:59:37 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+$email=$_POST["email"];
+$birthday=$_POST["birthday"];
+$mobile=$_POST["mobile"];
+$university=$_POST["university"];
+$branch=$_POST["branch"];
+$year=$_POST["year"];
+$cgpa=$_POST["cgpa"];
+echo " $fname </br>
+        $lname </br>
+        $email </br>
+        $birthday </br>
+        $mobile </br>
+       $university </br>
+       $branch </br>
+       $year </br>
+       $cgpa </br>";
+?>
