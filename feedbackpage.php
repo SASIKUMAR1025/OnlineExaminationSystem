5bd654043004571f5ce5a9f2fc31688861c64079
SASIKUMAR1025 <72292578+SASIKUMAR1025@users.noreply.github.com>
2022-02-09 15:39:13 +0530

    Create feedbackpage.php

---
+++ feedbackpage.php
@@ -0,0 +1,8 @@
+<?php
+$email=$_POST["email"];
+$mobile=$_POST["mobile"];
+$subject=$_POST["subject"];
+echo "<p>In the email text box,you entered: $email </p>
+       <p>In the mobile text box,you entered: $mobile </p>
+       <p>In the subject text box,you entered: $subject</p>";
+?>
